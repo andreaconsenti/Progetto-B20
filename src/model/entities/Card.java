4144a01804a533984d1e51b01ea4e3e9bc161121
Alessandro Carbone <alessandro.carbone02@universitadipavia.it>
2020-05-29 19:54:09 +0200

    Aggiunto metodo printCard

--- src/model/entities/Card.java
+++ src/model/entities/Card.java
@@ -4,10 +4,12 @@ public class Card {
 	
 	private FIGURE figure;
 	private Territory territory;
+	private int id;
 	
-	public Card(FIGURE figure, Territory territory) {
+	public Card(FIGURE figure, Territory territory, int id) {
 		this.figure = figure;
 		this.territory = territory;
+		this.id=id;
 	}
 
 	public FIGURE getFigure() {
@@ -18,5 +20,14 @@ public class Card {
 		return territory;
 	}
 	
+	public int getId() {
+		return id;
+	}
 	
+	public void printCard() {
+		if(territory!=null)
+			System.out.println("La carta di "+ this.territory.getName() +" ha come simbolo un: " +this.figure + "\n");
+		else
+			System.out.println("La tua carta è un Jolly\n");
+	}
 }

158fdd54f42c159b7ac59a8a31d1155a71180492
Alessandro Carbone <alessandro.carbone02@universitadipavia.it>
2020-05-30 16:34:28 +0200

    Aggiunto il file reader per continenti.txt e sistemato il file reader che genera gli obiettivi

--- src/model/util/FileHandler.java
+++ src/model/util/FileHandler.java
@@ -18,10 +18,11 @@ public class FileHandler {
 	private ArrayList<Territory> list;
 	private ArrayList<Card> card;
 	private ArrayList<Mission> missions;
-//	private static ArrayList<Continent> continents = new ArrayList<Continent>();
+	private ArrayList<Continent> continents;
 	private int nLine = 0;
 	private int nLineC=0;
 	private int nLineM=0;
+	private int nLineCont=0;
 	
 	// Method that generates an ArrayList which contains all territories
 	public ArrayList<Territory> genTerritories(String path) throws NumberFormatException, IOException {
@@ -147,16 +148,44 @@ public class FileHandler {
 		return false;
 	}
 	
-/*	private void genContinents() {
-		continents.add(new Continent("NordAmerica", 5));
-		continents.add(new Continent("SudAmerica", 2));
-		continents.add(new Continent("Europa", 5));
-		continents.add(new Continent("Africa", 3));
-		continents.add(new Continent("Asia", 7));
-		continents.add(new Continent("Australia", 2));
+	public ArrayList<Continent> genContinents (String path) throws NumberFormatException, IOException {
+		
+		BufferedReader in = new BufferedReader(new FileReader(path));
+		String line;
+		
+		continents = new ArrayList<Continent>();
+		int n = Integer.parseInt(in.readLine());
+		
+		String name;
+		int bonus;
+		int code;
+			
+		for(int i = 0; i<n; i++) {
+			
+			line = in.readLine();
+			code = Integer.parseInt(line.substring(0,2));
+			StringTokenizer st = new StringTokenizer (line.substring(2));
+			name = st.nextToken();
+			bonus = Integer.parseInt(st.nextToken());
+			
+			if(!addContinents(new Continent(code, name, bonus))) {
+				break;
+			}
+		}
+		in.close();
+		return continents;
 	}
-*/		
-	public ArrayList<Mission> genMissions(String path /*, ArrayList<Continent> continents*/) throws NumberFormatException, IOException{
+	
+	public boolean addContinents(Continent c) {
+		if(nLineCont <= list.size()) {
+			continents.add(c);
+			nLineCont++;
+			return true;
+		}
+		return false;
+	}
+	
+	public ArrayList<Mission> genMissions(String path, ArrayList<Continent> continents) throws NumberFormatException, IOException{
 		
 		BufferedReader in= new BufferedReader(new FileReader(path));
 		String line;
@@ -170,8 +199,8 @@ public class FileHandler {
 		Continent cont2 = null;
 		int codeMission;
 		int typeMission;
-		String name;
-		String name2;
+		int code;
+		int code2;
 		int j;
 		
 		
@@ -188,27 +217,15 @@ public class FileHandler {
 						break;
 					}
 					break;
-				
 				case 2:
-					name=st.nextToken();
-					/*for(Continent c : continents){
-						if(name==c.getName()) {
-							cont1=c;
-						}
-					}*/
-					name2=st.nextToken();
-					/*for(Continent c : continents) {
-						if(name2==c.getName()) {
-							cont2=c;
-						}
-					}*/
-					
-					if(!addMission(new Mission(name, name2, false, codeMission))) {
+					code=Integer.parseInt(st.nextToken());
+					code2=Integer.parseInt(st.nextToken());
+					cont1=continents.get((code)-1);
+					cont2=continents.get((code2)-1);
+					if(!addMission(new Mission(cont1, cont2, false, codeMission))) {
 						break;
 					}
-					
 					break;
-					
 			}	
 		}
 		in.close();
@@ -241,28 +258,40 @@ public class FileHandler {
 		}
 	}
 	
+	public void printContinents(ArrayList<Continent> continents) {
+		for(Continent cont: continents) {
+			System.out.println(cont.getName());
+		}
+	}
+	
 	public void printMissions(ArrayList<Mission> missions) {
 		for(Mission m : missions) {
 			m.printMission();
 		}
 	}
 	
-//	public static void main(String[] args) throws NumberFormatException, IOException {
-//		FileHandler f = new FileHandler();
-//		ArrayList<Territory> lista = f.genTerritories("assets/TerritoriEConfini.txt");
-////		System.out.println(lista.get(0));		//Commentato perché in output faceva uscire 2 volte "Alaska"  @author AleCarbo
-//		f.printTerritories(lista);
-//		System.out.println("----------------------\n");
-//		ArrayList<Territory> listaCompleta = f.addConfinanti(lista, "assets/confini.txt");
-//		for(Territory t : listaCompleta) {
-//			t.printConfini();
-//		}
-//		System.out.println("-----CARTE-----\n");
-//		ArrayList<Card> carte=f.genCards(lista, "assets/carte.txt");
-//		f.printCards(carte);
-//		System.out.println("-----OBIETTIVI-----\n");
-////		f.genContinents();
-//		ArrayList<Mission> missioni=f.genMissions("assets/obiettivi.txt"/*, continents*/);
-//		f.printMissions(missioni);
-//	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		FileHandler f = new FileHandler();
+		System.out.println("-----TERRITORI-----\n");
+		ArrayList<Territory> lista = f.genTerritories("assets/TerritoriEConfini.txt");
+//		System.out.println(lista.get(0));		//Commentato perché in output faceva uscire 2 volte "Alaska"  @author AleCarbo
+		f.printTerritories(lista);
+		System.out.println("-----CONFINI-----\n");
+		ArrayList<Territory> listaCompleta = f.addConfinanti(lista, "assets/confini.txt");
+		for(Territory t : listaCompleta) {
+			t.printConfini();
+		}
+		System.out.println("-----CARTE-----\n");
+		ArrayList<Card> carte=f.genCards(lista, "assets/carte.txt");
+		f.printCards(carte);
+		System.out.println("-----CONTINENTI-----");
+		ArrayList<Continent> continenti=f.genContinents("assets/continenti.txt");
+		f.printContinents(continenti);
+		
+		System.out.println("\n-----OBIETTIVI-----\n");
+//		f.genContinents();
+		ArrayList<Mission> missioni=f.genMissions("assets/obiettivi.txt", continenti);
+		f.printMissions(missioni);
+		
+	}	
 }

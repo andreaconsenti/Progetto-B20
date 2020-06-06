fd3669856621c5b3a02645cf59c6fcd94f646251
lucafornasari <luca.fornasari02@universitadipavia.it>
2020-06-06 15:31:31 +0200

    Bug fix

--- src/controller/GameSceneController.java
+++ src/controller/GameSceneController.java
@@ -14,8 +14,12 @@ import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import model.entities.COLOR;
 import model.entities.Player;
 import model.entities.PlayersList;
@@ -38,6 +42,8 @@ public class GameSceneController {
 	
 	RisikoGame game; 
 	HashMap<Territory, ArrayList<Pixel>> mappa;
+	PixelReader pixelReader;
+	PixelWriter pixelWriter;
 	
 	public void initialize() throws NumberFormatException, IOException{
 		game = new RisikoGame(PlayersList.getPlayers());
@@ -47,6 +53,23 @@ public class GameSceneController {
 		territoryLabel.setOpacity(0);
 		
 		mappa = ImageAssets.imageProcess(image, game.getTerritories());
+		
+//		pixelReader = map.getImage().getPixelReader();
+//		WritableImage wImage = new WritableImage(
+//				 (int) map.getImage().getWidth(),
+//				(int) map.getImage().getHeight());
+//		pixelWriter = wImage.getPixelWriter();
+//		for (int y = 0; y < image.getHeight(); y++){
+//			
+//				for (int x = 0; x < image.getWidth(); x++){
+//					
+//					Color color = pixelReader.getColor(x, y);
+//					pixelWriter.setColor(x,y,color);
+//				}
+//				
+//		}
+//		map.setImage(wImage);
+
 		
 	}
 	
@@ -85,9 +108,48 @@ public class GameSceneController {
 	
 	
 	
+//	private void changeColor(ArrayList<Pixel> list) {
+//		for (int y = 0; y < getMaxY(list); y++)
+//		{
+//		for (int x = 0; x < getMaxX(list); x++)
+//		{
+//		Color color = pixelReader.getColor(x, y);
+//		double r = color.getRed() / 1.5;
+//		double g = color.getGreen() / 1.5;
+//		double b = color.getBlue() / 1.5;
+//		int red = (int) (r * 255);
+//		int green = (int) (g * 255);
+//		int blue = (int) (b * 255);
+//		color = Color.rgb(red,green,blue);
+//		pixelWriter.setColor(x,y,color);
+//		}
+//		}
+//		imageView.setImage(wImage);
+//
+//	}
 	
 	private String territoryText(Territory t) {
 		return "Territorio: " + t.getName();
+	}
+	
+	private int getMaxX(ArrayList<Pixel> l) {
+		int x = 0;
+		for(Pixel p : l) {
+			if (p.getX() > x) {
+				x = p.getX();
+			}
+		}
+		return x;
+	}
+	
+	private int getMaxY(ArrayList<Pixel> l) {
+		int y = 0;
+		for(Pixel p : l) {
+			if (p.getX() > y) {
+				y = p.getY();
+			}
+		}
+		return y;
 	}
 	
 	

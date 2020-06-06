af381d52dfe4d72bcedf2d850c24ab42baedc63f
lucafornasari <luca.fornasari02@universitadipavia.it>
2020-06-06 12:31:35 +0200

    Update gamescene

--- src/controller/GameSceneController.java
+++ src/controller/GameSceneController.java
@@ -11,8 +11,11 @@ import javax.imageio.ImageIO;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import model.entities.COLOR;
 import model.entities.Player;
 import model.entities.PlayersList;
@@ -27,6 +30,12 @@ public class GameSceneController {
 	@FXML
 	ImageView map;
 	
+	@FXML
+	Pane gamePane;
+	
+	@FXML
+	Label territoryLabel;
+	
 	RisikoGame game; 
 	HashMap<Territory, ArrayList<Pixel>> mappa;
 	
@@ -35,30 +44,50 @@ public class GameSceneController {
 		File img = new File("src/view/fxmls/images/Territory_Color2.png");
 		BufferedImage image = ImageIO.read(img ); 
 		
+		territoryLabel.setOpacity(0);
+		
 		mappa = ImageAssets.imageProcess(image, game.getTerritories());
 		
 	}
 	
-	public void mapClicked(MouseEvent e) {
+	
+	
+	public void mouseMoved(MouseEvent e) {
 		int x =  (int)e.getX();
 		int y =  (int)e.getY();
+		int check;
 		
 		for(Territory t : game.getTerritories()) {
 			
+			check = 0;
 			for(Pixel p : mappa.get(t)) {
 				
 				if((p.getX() == x) && (p.getY() == y)) {
-					System.out.println(t.toString());
+					check = 1;
+					territoryLabel.setOpacity(100);
+					territoryLabel.setText(territoryText(t));
+					break;
+				} else {
+					territoryLabel.setOpacity(0);
 				}
 				
 			}
+			if(check == 1) {
+				break;
+			}
+			
 			
 			
 		}
-		
-		
-		
-		
+	
+	}
+	
+	
+	
+	
+	
+	private String territoryText(Territory t) {
+		return "Territorio: " + t.getName();
 	}
 	
 	

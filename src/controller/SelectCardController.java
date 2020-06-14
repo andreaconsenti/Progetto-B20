08f35f204f0b4c793d3875ec7b0f1de644567111
AlessandroGenta <alessandro.genta02@universitadipavia.it>
2020-06-14 14:37:18 +0200

    cardScene

--- src/controller/SelectCardController.java
+++ src/controller/SelectCardController.java
@@ -1,5 +1,96 @@
 package controller;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class SelectCardController {
+	
+	@FXML
+	private ImageView carta1;
+
+	@FXML
+	private ImageView carta2;
+	
+	@FXML
+	private ImageView carta3;
+
+	@FXML
+	private Button scambiaButton;
+
+	@FXML
+	private Button annullaButton;
+	
+	@FXML
+	private ImageView cardImage0;
+
+	@FXML
+	private Label territoryName0;
+
+	@FXML
+	private ImageView cardImage01;
+
+	@FXML
+	private Label territoryName01;
+
+	@FXML
+    private ImageView cardImage02;
+
+    @FXML
+    private Label territoryName02;
+
+    @FXML
+    private ImageView cardImage021;
+
+    @FXML
+    private Label territoryName021;
+
+    @FXML
+    private ImageView cardImage022;
+
+    @FXML
+    private Label territoryName022;
+
+    @FXML
+    private ImageView cardImage023;
+
+    @FXML
+    private Label territoryName023;
+
+    @FXML
+    private ImageView cardImage024;
+
+    @FXML
+    private Label territoryName024;
+
+    @FXML
+    private ImageView cardImage025;
+
+    @FXML
+    private Label territoryName025;
+
+
+	/*void addCard() {
+		//aggiungere stackPane
+		//la prima è cardImage0 e territoryName0, la seconda è cardImage01 e territoryName01, poi 02 e 02, etc...
+		
+	}
+	
+	
+	
+	@FXML
+	void onAnnullaPressed(ActionEvent event) {
+		//cancella le 3 carte dal posto
+	}
+
+
+	@FXML
+	void onScambiaPressed(ActionEvent event) {
+		//verifica e fa combinazione
+	}*/
 
 }
+
+

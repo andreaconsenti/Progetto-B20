1ac83629b2998a9fcc70102c5f40056c5d6a612d
Alessandro Carbone <alessandro.carbone02@universitadipavia.it>
2020-06-20 16:58:05 +0200

    Aggiornamento nomi variabili

--- src/controller/SelectCardController.java
+++ src/controller/SelectCardController.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -9,96 +8,57 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import model.entities.RisikoGame;
 import model.entities.RisikoGame.GAME_PHASE;
-import model.entities.Territory;
-import model.util.Pixel;
 
 public class SelectCardController {
-	
-	@FXML
-	private ImageView carta1;
-
-	@FXML
-	private ImageView carta2;
-	
-	@FXML
-	private ImageView carta3;
-
-	@FXML
-	private Button scambiaButton;
-
-	@FXML
-	private Button annullaButton;
-	
-	@FXML
-	private ImageView cardImage0;
-
-	@FXML
-	private Label territoryName0;
-
-	@FXML
-	private ImageView cardImage01;
-
-	@FXML
-	private Label territoryName01;
-
-	@FXML
-    private ImageView cardImage02;
 
     @FXML
-    private Label territoryName02;
+    private ImageView cardImage0;
 
     @FXML
-    private ImageView cardImage021;
+    private Label territoryLabel0;
 
     @FXML
-    private Label territoryName021;
+    private ImageView cardImage01;
 
     @FXML
-    private ImageView cardImage022;
+    private Label territoryLabel1;
 
-    @FXML
-    private Label territoryName022;
-
-    @FXML
-    private ImageView cardImage023;
-
-    @FXML
-    private Label territoryName023;
-
-    @FXML
-    private ImageView cardImage024;
-
-    @FXML
-    private Label territoryName024;
-
-    @FXML
-    private ImageView cardImage025;
-
-    @FXML
-    private Label territoryName025;
-    
     @FXML
     private Pane paneScambioCarte;
+
+    @FXML
+    private ImageView carta3;
+
+    @FXML
+    private Button scambiaButton;
+
+    @FXML
+    private Button annullaButton;
+
+    @FXML
+    private ImageView carta1;
+
+    @FXML
+    private ImageView carta2;
 
     @FXML
     void initialize() {
     	paneScambioCarte.setOpacity(0.35);
     	scambiaButton.setDisable(true);
     	annullaButton.setDisable(true);
-    	if(GameSceneController.game.getGamePhase()==GAME_PHASE.BATTLE) {
+    	if(GameSceneController.game.getGamePhase()==GAME_PHASE.REINFORCEMENT) {
     		paneScambioCarte.setOpacity(1);
     		scambiaButton.setDisable(false);
     		annullaButton.setDisable(false);
     	}
     }
     
-//	void addCard() {
-//		//aggiungere stackPane
-//		//la prima è cardImage0 e territoryName0, la seconda è cardImage01 e territoryName01, poi 02 e 02, etc...
-//		
-//	}
+	void addCard() {
+		//aggiungere stackPane
+		//la prima è cardImage0 e territoryName0, la seconda è cardImage01 e territoryName01, poi 02 e 02, etc...
+		
+	}
 	
 	@FXML
 	void onAnnullaPressed(ActionEvent event) throws IOException {
